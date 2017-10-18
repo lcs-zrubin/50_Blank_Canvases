de65187a93caf7cec8e93253c14c380b7eb52a2a
Rubin Zach <zrubin@lcs-mba3136.lcs.on.ca>
2017-10-18 10:19:25 -0400

    Looked at random noise generation

--- 50 Blank Canvases.playground/Pages/Canvas 1.xcplaygroundpage/Contents.swift
+++ 50 Blank Canvases.playground/Pages/Canvas 1.xcplaygroundpage/Contents.swift
@@ -13,7 +13,7 @@ import Cocoa
 import PlaygroundSupport
 
 // Create canvas
-let canvas = Canvas(width: 400, height: 300)
+let canvas = Canvas(width: 200, height: 100)
 
 /*:
  ## Add your code below
@@ -22,8 +22,32 @@ let canvas = Canvas(width: 400, height: 300)
  
  Use whitespace and comments as appropriate.
  */
-// Replace this comment with your first comment – what is the goal of the code you're about to write?
+// Illustrate random niose generation.
+for x in stride(from: 0, through: 200, by: 1) {
+    
+    //Decide how tall to draw the line
+    let toY = random(from: 0, toButNotIncluding: 101)
+    
+    //Draw a vertical line
+    canvas.drawLine(fromX: x, fromY: 0, toX: x, toY: toY)
+}
 
+//Learn about data types
+var name = "Lakefield" // assignment statement
+name = "Lakefield College School"
+
+// Create a number
+let value = 4 // type inference guesses integer
+let anotherValue : Int = 5
+
+//Create a number that is not an integer
+var decimalValue = 4.5
+decimalValue = 5
+let anotherDecimalValue : Double = 5.5
+
+// What are the maximum and minimum possible values?
+Int.min
+Int.max
 
 
 /*:
